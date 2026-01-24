690c69229ff7310f6a8bc4b67930ed6a20736de8
Jaiganesh <jaiganesh0517@gmail.com>
2026-01-24 14:02:31 +0530

    Add area calculation methods for different polygons
    Implement methods to calculate areas of circle, rectangle, and square. Fix method calls in main.

---
+++ Areas.java
@@ -0,0 +1,25 @@
+//This File will contains the code that implements the logic of calculating the Areas of different Polygon
+class Area{
+     //Circles Area
+    public static float csArea(int radius){
+          int cArea = 3.14 * radius * radius;
+          return cArea;
+    }
+// Ractangle Area
+    public static int rArea(int l,int b){
+         int rArea = l * b;
+          return rArea;
+    }
+// Square Area
+    public static int sArea(int a){
+         int sArea = a * a;
+          return sArea;
+    }
+    public static void main(Strring [] args){
+    System.out.println(cArea(3.0);
+    System.out.println(4,3);
+    System.out.println(4);
+    }
+}
+   
+
