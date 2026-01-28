126cd3d3b0c0e0eb7ef6c297ebe4f0ad6606f355
Jaiganesh <jaiganesh0517@gmail.com>
2026-01-28 23:56:14 +0530

    Methods in java
    Here we understand about the methods in java it's syntax and how function call is made by using a example.

--- Arrays.java
+++ Arrays.java
@@ -1,1 +1,21 @@
-/*In this section we will learn about 
+/*In this section we will learn about methods(similar to functions) in java 
+ function are basically a way that creates a easy way to doing Task.
+ and it also contribute to the cleanliness of the code*/
+
+/*Syntax 
+ accessModifier returnType methodName(perimeter){}
+ We always write method  in between the class and main method this the standard way of writing methods. 
+ we call the main from inside the main method */
+
+
+//Sum method for addition of two numbers 
+class Methods{
+   public static int sum(int a,int b){// method 
+      return a + b; 
+   }
+   public static void main(String [] args){
+      int a = 2;
+      int b = 4;
+      System.out.println(sum(a,b)); //method call
+   } 
+} 
