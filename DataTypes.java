200892bd260a01271111c2e67d6d6fa1b8b55a0a
Jaiganesh <jaiganesh0517@gmail.com>
2026-02-03 19:23:42 +0530

    Refactor Operation class to showcase data types
    Updated the Operation class to demonstrate data types with print statements instead of arithmetic operations.

---
+++ DataTypes.java
@@ -0,0 +1,19 @@
+// This program will let you know about the Data types and Operator in java with some operation.
+class Operation { 
+       public static void main(String [] args){
+        //Data Types
+        int a = 10; //integer
+        System.out.println("Integer "+a);
+        float b = 10.6;//Floating Point number
+        System.out.println("Floating Point Number "+b);
+        boolean bl = true;//Boolean
+        System.out.println("Boolean "+bl);
+        long l = 1000000;//long similar to integer but greater in range
+        System.out.println("long "+l);     
+        char ch = 'a';//Character for inputing alphabets
+        System.out.println("Characters "+ch);
+        String str = "StringDataType;//For taking input of strings
+        System.out.println("String "+str);     
+       }
+}
+      
