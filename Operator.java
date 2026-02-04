db51b005e2cb9554a63a5f0493e4878c99de8b81
Jaiganesh <jaiganesh0517@gmail.com>
2026-02-04 23:49:21 +0530

    Rename class from Operatirs to Operator

---
+++ Operator.java
@@ -0,0 +1,22 @@
+class Operator{
+  public static void main(String [] args){
+     // Assignment Operator --> = is used to assign value in any variable
+     int a = 5; 
+     // Arithmetic Operator --> + , - , * , / , % is used to perform mathematical operation 
+     int a = 10;
+     int b = 9;
+     System.out.println( a + b ); // 19
+     System.out.println( a - b ); // 1
+     System.out.println( a * b ); // 90
+     System.out.println( a / b ); // 1
+     System.out.println( a % b ); // 1
+     /* BODMAS its a very and useful concept that illustrate how to decide in which order we have to perform 
+     B--> BRACKET
+     O--> ORDER
+     D--> DIVIDE
+     M--> MULTIPLY
+     A--> ADDITION
+     S--> SUBTRACTION */
+     System.out.println( 9 / 3 / 3 ); // 1
+     System.out.println( 9 / ( 3 / 3 )); // 9
+     System.out.println( 9 / (3 / 3  ); // 3
