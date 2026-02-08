e3d3196c8e91e5ce8473ee2ca8c2aadf168322f9
Jaiganesh <jaiganesh0517@gmail.com>
2026-02-08 22:37:40 +0530

    Create Arrays.java
    define array and traverse it

---
+++ Arrays.java
@@ -0,0 +1,12 @@
+/* Arrays is a linear data structure which basically stores values in a contiguous manner. it's zero index base. and it's not resizable once created.*/
+
+//Store the fist 10 natural numbers and them(traverse)
+public class Array{
+     public static void main(String [] args){
+     int []arr = new int[10];//initialization and declaration 
+     for(int i = 0; i<arr.length;i++){
+         System.out.println(arr[i]);
+     }
+    }
+}
+        
