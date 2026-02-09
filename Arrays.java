2a89bc95a2dcb3fe59224a2262791ad59697e5ab
Jaiganesh <jaiganesh0517@gmail.com>
2026-02-09 19:58:04 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -4,7 +4,7 @@
 public class Array{
      public static void main(String [] args){
      int []arr = new int[10];//initialization and declaration 
-     for(int i = 0; i<arr.length;i++){
+     for(int i = 0; i<arr.length;i++){//traversing
          System.out.println(arr[i]);
      }
     }
