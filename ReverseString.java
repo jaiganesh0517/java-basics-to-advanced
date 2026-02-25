02425c0864760be2600b9355a0208c8f3951a53d
Jaiganesh <jaiganesh0517@gmail.com>
2026-02-25 22:37:18 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,7 @@
+class ReverseString{
+   public static void main(String [] args){
+   String s = "hello";
+   String rev = new StringBuilder(s).reverse().toString();
+   System.out.println(rev);
+  }
+}
